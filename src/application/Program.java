15734e72dd1aff41d017d69d0aca78864d4db949
FellipeG <fellipegarciasrj@yahoo.com.br>
2018-12-21 16:21:51 -0200

    Solving the problem without default methods

---
+++ src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.security.InvalidParameterException;
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.services.UsaInterestService;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		try {
+		System.out.print("Amount: ");
+		double amount = sc.nextDouble();
+		System.out.print("Months: ");
+		int months = sc.nextInt();
+		
+		UsaInterestService is = new UsaInterestService(1.0);
+		double payment = is.payment(amount, months);
+		
+		System.out.println("Payment after: " + months + " months");
+		System.out.println(String.format("%.2f", payment));
+		}
+		catch (InvalidParameterException e) {
+			System.out.println("Invalid Parameter: " + e.getMessage());
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Input error: " + e.getMessage());
+		}
+		finally {
+			sc.close();
+		}
+	}
+
+}
